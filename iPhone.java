f9f74c8c0f9cc80a0031f4f0420b137a77bf6525
Gilbert Green <42986583+gibsongreen@users.noreply.github.com>
2019-04-26 17:53:31 -0400

    Update iPhone.java

--- iPhone.java
+++ iPhone.java
@@ -1,32 +1,60 @@
 // Gibson Green
 // Integration Project
+/**iPhone class extends the Apple Product class and is a class describing iPhones.
+ * @author gibso
+ *
+ */
 public class iPhone extends AppleProducts {
-  protected String iPhoneColor;
+  /** what is the iPhone color.
+   * 
+   */
+  protected String iphoneColor;
+  /** gives the iPhone storage.
+   * 
+   */
   protected int iPhoneStorageSize;
 
+  /** gets the iPhone color.
+   * @return iPhone color
+   */
   public String getiPhoneColor() {
-    return iPhoneColor;
+    return iphoneColor;
   }
 
+  /** sets the iPhone color.
+   * @param iPhoneColor
+   */
   public void setiPhoneColor(String iPhoneColor) {
-    this.iPhoneColor = iPhoneColor;
+    this.iphoneColor = iPhoneColor;
   }
 
+  /** gets the iPhone storage size.
+   * @return storage size
+   */
   public int getiPhoneStorageSize() {
     return iPhoneStorageSize;
   }
 
+  /** sets the iPhone storage size.
+   * @param iPhoneStorageSize
+   */
   public void setiPhoneStorageSize(int iPhoneStorageSize) {
     this.iPhoneStorageSize = iPhoneStorageSize;
   }
   
   //overriden communicate method
+  /* (non-Javadoc)
+   * @see AppleProducts#communicate()
+   */
   public void communicate() {
     System.out.println("This iPhone is making a phone call.");
   }
+  
   //overriden product type method
+  /* (non-Javadoc)
+   * @see AppleProducts#showProductType()
+   */
   public void showProductType() {
     System.out.println("This is a iPhone.");
   }
 }
-
